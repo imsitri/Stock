882aad789417b82729b814c4ff2397282fa2fd69
imsitri <sitri241002@gmail.com>
2024-03-28 19:54:30 -0700

    fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://imsitri.github.io',
-  base: '/Stock',
+  site: 'https://imsitri.github.io/Stock',
+  base: '',
 })
